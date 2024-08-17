b6df179223aa1cb9461677da7bd29b2a621ddde5
백현민 <bhm01@naver.com>
2024-08-18 04:13:31 +0900

    feat : 프로젝트 신규 생성
    오이트립 프로젝트 신규 생성

---
+++ src/main/java/com/oetrip/oe/OetripApplication.java
@@ -0,0 +1,13 @@
+package com.oetrip.oe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OetripApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OetripApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/oetrip/oe/OetripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oetrip.oe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OetripApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
